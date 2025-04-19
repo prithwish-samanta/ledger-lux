e9d5b521fd426379c04dde314f8f5ad6b2d50f30
prithwish <wprith@gmail.com>
2025-04-19 12:02:37 +0530

    initial commit

---
+++ src/main/java/dev/prithwish/ledgerlux/LedgerLuxApplication.java
@@ -0,0 +1,13 @@
+package dev.prithwish.ledgerlux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LedgerLuxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LedgerLuxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/prithwish/ledgerlux/LedgerLuxApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.prithwish.ledgerlux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LedgerLuxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
